2a2a9b90fbf8f8b22f3ae1fcd0296d7468981852
beresluca <71323525+beresluca@users.noreply.github.com>
2020-10-27 17:03:20 +0100

    raw version for a subset of subjects

--- FGsubject (1).m
+++ FGsubject (1).m
@@ -1,28 +1,49 @@
 % get all subject files --> to later perform stats
 % should look like an N dimensional struct with each layer of subject data
 
-% tmp = FGdata_clean('sub5Log.mat', 0);
-% tmp(2) = tmp(1);
-% size(tmp) 
-% ans = 
-%       1    2
-%
-%  tmp(2).subRT(:,1,1,1) --> could be indexed like this 
+searchDir = '/C:/Users/Luca/**/SFG_sample/';
+subjects = 2:5; % right now works on only a subset of subjects
 
-subjects = 2:5;
-
-SubLogFiles = dir('**/sub*Log.mat');
-m = (struct2cell(SubLogFiles));
-filePaths = m(1,:);
-filePaths_reshaped = reshape(filePaths, [numel(filePaths), 1]);
-filePaths_reshaped = string(filePaths_reshaped);
-
-
-for i = 1 : length(SubLogFiles)
+%% Find the files somehow - use "dir" perhaps
+filePaths = strings(length(subjects), 1);  % preallocate
+for s = subjects
+    searchDir; % not sure how to use this for the next line of code
+    SubLogFiles = dir(['**/sub', num2str(s), 'Log.mat']);
+    %%%%%%%%%%
+    % here make sure somehow that only one result was returned by "dir"
+    %%%%%%%%%%
+    if numel(SubLogFiles) > 1
+        SubLogFiles(2)= [];
+    end
+    % deletes the second element of the structure (if there is any) to
+    % avoid duplicates (probably not the best solution)
     
-    baseFileName = SubLogFiles(i).name;
-    fullFileName = fullfile(SubLogFiles(i).folder, baseFileName);
-    FGdata_clean(baseFileName);
+    % store path
+    filePaths(s) = fullfile(SubLogFiles.folder, SubLogFiles.name);
     
 end
+
+%% Hopefully the above block gave us a correct list of files to load - now
+% we can just go through them in a loop
+
+% First preallocate all result variables - we would like to have variables
+% capable of holding all subject-level results
+FGall = struct('ToneCompValues', zeros(1,2), 'figCoherence', zeros(1,2),...
+        'subRT', zeros(20,10,2,2), 'subAcc', zeros(20,10,2,2),...
+        'mean_RT', zeros(1,1), 'sd_RT', zeros(1,1), 'RTblockMeanSD', zeros(2,10,2,2),...
+        'mean_stmType', zeros(2,2), 'sd_stmType', zeros(2,2), 'accuracy', zeros(1,1),...
+        'MeanAccuracy', zeros(2,2), 'MeanAccuracy_block', zeros(10,2,2));
+
+
+for s = subjects
+    FGall(s) = FGdata_clean(filePaths(s), 0);
+end
+
+% for i = 1
+%     
+%     baseFileName = SubLogFiles(i).name;
+%     fullFileName = fullfile(SubLogFiles(i).folder, baseFileName);
+%     FG_all = FGdata_clean(baseFileName, 0);
+%     
+% end
 
