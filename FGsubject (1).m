ba2ccc772a7930715943ff21b9c6b25d7d8f5c24
beresluca <71323525+beresluca@users.noreply.github.com>
2020-10-29 14:07:02 +0100

    put it in a function, added help
    also added subject number for the struct --> updated version of FGdata_clean is in the repo

--- FGsubject (1).m
+++ FGsubject (1).m
@@ -1,66 +1,114 @@
-% get all subject files --> to later perform stats
-% should look like an N dimensional struct with each layer of subject data
+function FGall = FGsubject(searchDir, subjects)
+%% SFG aging study - behavioral data analysis for multiple subjects using the 'FGdata_clean' function 
+%
+% USAGE: FGall = FGsubject(searchDir, subjects)
+%
+% Function for getting multiple subject data at once, using the
+% "FGdata_clean' function (see help for more info, default is no plots)
+%
+% Mandatory input 
+%
+% searchDir:             - char array, directory for subject log files
+%                         ** wildcards not supported!
+%                         (example: /home/usr/folder1/folder2/) 
+%
+% subjects:              - numeric array of subject no. 
+%                         (example: [2:5 101 103:105])
+%
+% Output
+%
+% ToneCompValues:       - Numeric vector with tone component values
+%
+% figCoherence:         - NUmeric vector with figure coherence values  
+% 
+% subRT:                - 4D array with block-level RT sorted by stimulus type 
+%                           [trial x block x figure x difficulty]*
+%
+% subAcc:               - 4D array with block-level accuracy sorted by stimulus type
+%                           [trial x block x figure x difficulty]*
+%
+% mean_RT:              - Numeric value, overall mean of RT
+%
+% sd_RT:                - Numeric value, overall Standard Deviation of RT
+%
+% RTblockMeanSD:        - 4D array with block-level means (1st row of each dimension) 
+%                           and SD of RT (2nd row of each dimension)
+%                           [mean,SD x block x figure x difficulty]*
+% 
+% mean_stmType:         - 2x2 array with mean RTs for each stimulus type
+%                           [figure x difficulty]*
+% 
+% sd_stmType:           - 2x2 array with SD RTs for each stimulus type
+%                           [figure x difficulty]*
+% 
+% accuracy:             - Proportion of correct responses (%)
+% 
+% MeanAccuracy:         - 2x2 array with mean accuracy(%) for each stimulus type
+%                           [figure x difficulty]*
+% 
+% MeanAccuracy_block:   - 3D array with block-level accuracy(%) sorted by stimulus type
+%                           [block x figure x difficulty]*
+%                          
+%
+%
+%  *  block no. [1:10]
+%     figure (1=absent, 2=present)
+%     difficulty (1=easy, 2=difficult)
+%
 
-% searchDir = '/home/lucab/Downloads/FG-RT-master/SFG_sample/';
-searchDir = '/home/stim/FG-RT/sampleData/';  % adam's home pc path for this
-%searchDir = '/C:/Users/Luca/**/SFG_sample/';
+%% Check inputs
 
-% subjects = [2:5 101 103:105]; % right now works on only a predefined subset of subjects, 
-%                                 % need to correct for errors when subject no. is missing
-%                                 % (??)
-% What do you mean by "when subject no. is missing"?
+% check number of inputs
+if ~ismember(nargin, 2) 
+    error('Function FGsubject requires input arg "searchDir" AND "subjects"!');
+end
 
-subjects = [3, 4];  % for sampleData in repo
+% check for searchDir
+if ~exist(searchDir, 'dir')
+    error('Input arg "searchDir" is not an existing directory!');
+end
 
+% check for subjects
+if ~isnumeric(subjects)
+    error('Input arg "subjects" is not a valid numeric array!');
+end
 
-%% Find the files somehow - use "dir" perhaps
+%% Find the files in given search directory
 
 filePaths = strings(length(subjects), 1);  % preallocate
-% a counter is used for storing paths in "filePaths" in the loop
 counter = 1;
-for s = subjects
-%     searchDir; % not sure how to use this for the next line of code ("too many input arg for dir")
-%     SubLogFiles = dir(['**/sub', num2str(s), 'Log.mat']);
 
-    SubLogFiles = dir([searchDir, '**/sub', num2str(s), 'Log.mat']);  % this version works for me - adam
+for s = subjects
     
-    %%%%%%%%%%
-    % here make sure somehow that only one result was returned by "dir"
-    %%%%%%%%%%
-%     if numel(SubLogFiles) > 1
-%         SubLogFiles(2)= [];
-%     end
-    % deletes the second element of the structure (if there is any) to
-    % avoid duplicates (probably not the best solution)
-    
-    % Good solution, but we could just make it throw an error if there are
-    % more than one file. Let the user handle the problem.
+    % get subject log files
+    SubLogFiles = dir([searchDir, '**/sub', num2str(s), 'Log.mat']);
+  
+    % check for duplicates
     if numel(SubLogFiles) > 1
-        error(['Multiple files found for subject ', num2str(s), '! Aborting as we are better safe then sorry!']);
+        error(['Multiple files found for subject ', num2str(s), '! Aborting as we are better safe than sorry!']);
     end    
-    
+
     % store path
     filePaths(counter) = fullfile(SubLogFiles.folder, SubLogFiles.name);
-    counter = counter+1;
+    counter = counter + 1;
     
 end
 
+%% Go through the file paths in a loop
 
-%% Hopefully the above block gave us a correct list of files to load - now
-% we can just go through them in a loop
-
-% First preallocate all result variables - we would like to have variables
-% capable of holding all subject-level results
-FGall = struct('ToneCompValues', zeros(1,2), 'figCoherence', zeros(1,2),...
-        'subRT', zeros(20,10,2,2), 'subAcc', zeros(20,10,2,2),...
+% preallocate struct
+FGall = struct('subNumber', zeros(1,1),'ToneCompValues', zeros(1,2), ...
+        'figCoherence', zeros(1,2), 'subRT', zeros(20,10,2,2), 'subAcc', zeros(20,10,2,2),...
         'mean_RT', zeros(1,1), 'sd_RT', zeros(1,1), 'RTblockMeanSD', zeros(2,10,2,2),...
         'mean_stmType', zeros(2,2), 'sd_stmType', zeros(2,2), 'accuracy', zeros(1,1),...
         'MeanAccuracy', zeros(2,2), 'MeanAccuracy_block', zeros(10,2,2));
 
+% loop through
 for s = 1:length(subjects)
+    
     FGall(s) = FGdata_clean(filePaths(s), 0);
+    
 end
 
-
-
+return
 
